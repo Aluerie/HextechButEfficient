58220b2e7ee6ac0b2c200e5a613fb0e4094a371c
Aluerie❤ <aluerie@outlook.com>
2023-09-21 21:08:58 +0300

    Update be_management.py

--- be_mastery/be_management.py
+++ be_mastery/be_management.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, TypedDict
+
+from typing import TYPE_CHECKING, Mapping, TypedDict
 
 from lcu_driver import Connector
 
@@ -9,65 +10,58 @@ if TYPE_CHECKING:
 connector = Connector()
 
 
-class ChampData(TypedDict):
-    name: str
-    level: int
-    tokens_earned: int
-    owned: bool
-    shards: int
-    full_shards: int
+async def work_func(connection, summoner):
+    summoner_id: int = (await summoner.json())["summonerId"]
+
+    # Champion ID - Mastery Level Mapping
+    champid_mlvl_map: Mapping = {}
+
+    req1 = await connection.request("get", f"/lol-collections/v1/inventories/{summoner_id}/champion-mastery")
+    for item in await req1.json():
+        champid_mlvl_map[item["championId"]] = item["championLevel"]
+
+    # Loot
+    req2 = await connection.request("get", "/lol-loot/v1/player-loot")
+
+    for item in await req2.json():
+        if item["type"] == "CHAMPION_RENTAL":
+            if item["itemStatus"] == "OWNED":
+                champ_id = item["storeItemId"]
+                if champ_id in champid_mlvl_map:
+                    if champid_mlvl_map[champ_id] == 7:
+                        save_shards = 0
+                    elif champid_mlvl_map[champ_id] == 6:
+                        save_shards = 1
+                    else:
+                        save_shards = 2
+                else:
+                    save_shards = 2
+            else:
+                save_shards = 3
+
+            disenchant_shards = max(0, item["count"] - save_shards)
+
+            if disenchant_shards:
+                req3 = await connection.request(
+                    "post",
+                    f"/lol-loot/v1/recipes/CHAMPION_RENTAL_disenchant/craft?repeat={disenchant_shards}",
+                    data=[item["lootName"]],
+                )
 
 
 @connector.ready
 async def connect(connection: Connection):
-    print('LCU API is ready to be used.')
-    summoner = await connection.request('get', '/lol-summoner/v1/current-summoner')
+    print("LCU API is ready to be used.")
+    summoner = await connection.request("get", "/lol-summoner/v1/current-summoner")
     if summoner.status != 200:
-        print('Please login into your account to change your icon and restart the script...')
+        print("Please login into your account to change your icon and restart the script...")
     else:
-        summoner_id: int = (await summoner.json())['summonerId']
-
-        req = await connection.request('get', f'/lol-champions/v1/inventories/{summoner_id}/champions')
-        champ_dict: dict[int, ChampData] = {  # champ_ownership
-            item['id']: {
-                'name': item['alias'],
-                'owned': item['ownership']['owned']
-            } for item in await req.json()
-        }
-
-        req = await connection.request('get', f'/lol-collections/v1/inventories/{summoner_id}/champion-mastery')
-        for item in await req.json():
-            d = champ_dict[item['championId']]
-            d['level'] = item['championLevel']
-            d['tokens_earned'] = item['tokensEarned']
-
-        req = await connection.request('get', '/lol-loot/v1/player-loot')
-        for item in await req.json():
-            if item["type"] == "CHAMPION_RENTAL":
-                d = champ_dict[item['storeItemId']]
-                d['shards'] = item['count']
-
-        print([{k: v} for k, v in champ_dict.items() if v.get('shards', 0)])
-
-
-
-        # print(champ_shards)
-
-        # req = await connection.request(
-        #     'post', '/lol-loot/v1/recipes/CHAMPION_RENTAL_disenchant/craft?repeat=1',
-        #     data=["CHAMPION_RENTAL_20"]
-        # )
-        # ctx_menu = await req.json()
-        # print(ctx_menu)
-
-        req = await connection.request('get', '/swagger/v1/api-docs')
-        d = await req.json()
-        print(d)
+        await work_func(connection, summoner)
 
 
 @connector.close
 async def disconnect(_: Connection):
-    print('The client have been closed!')
+    print("The client have been closed!")
 
 
 connector.start()
