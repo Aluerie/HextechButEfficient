1e7f82de48fed64e43af5a0d16985c45941d6c20
Aluerie❤ <aluerie@outlook.com>
2023-10-19 21:01:27 +0300

    Update base.py

--- src/my_connector/base.py
+++ src/my_connector/base.py
@@ -1,12 +1,17 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Awaitable, Callable
+from typing import TYPE_CHECKING, Protocol
 
 from lcu_driver import Connector
 
 if TYPE_CHECKING:
     from aiohttp import ClientResponse
     from lcu_driver.connection import Connection
+
+
+class WorkerFunc(Protocol):
+    async def __call__(self, connection: Connection, summoner: ClientResponse) -> None:
+        ...
 
 
 class AluConnector(Connector):
@@ -21,22 +26,13 @@ class AluConnector(Connector):
     >>> connector = AluConnector(worker_func)
     >>> connector.start()
 
-    Note that worker_func is supposed to be of the following signature:
-
-    >>> async def worker_func(connection: Connection, summoner: ClientResponse) -> None:
-
+    Note that worker_func is supposed to be of the WorkerFunction signature shown above^:
     worker_func is supposed to do the job in a script.
     """
 
-    def __init__(
-        self,
-        coro_func: Callable[[Connection, ClientResponse], Awaitable[None]],
-        *,
-        loop=None,
-    ):
+    def __init__(self, coro_func: WorkerFunc, *, loop=None):
         super().__init__(loop=loop)
-
-        self.coro_func: Callable[[Connection, ClientResponse], Awaitable[None]] = coro_func
+        self.coro_func: WorkerFunc = coro_func
         self._set_event("ready", self.connect)
         self._set_event("disconnect", self.disconnect)
 
